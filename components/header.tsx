7600084b774ed8ac25d098fc69c3e8cd53739272
Benjamin Bork <benjamin.bork@schifferberg.com>
2024-05-09 09:54:55 +0200

    inital project setup

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-backgroundContrast px-6 text-white">Apple Tv+</header>
+  );
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      background: "#000",
+      backgroundContrast: "#111",
+      textBlack: "#1d1d1f",
+      white: "#fff",
     },
+
+    extend: {},
   },
   plugins: [],
 };
